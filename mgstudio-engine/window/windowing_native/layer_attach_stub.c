4da25da515c01391e1e575f3a45e64e2d747aec7
Milky2018 <842376130@qq.com>
2026-05-08 09:37:58 +0800

    fix: align bloom2d render parity

--- mgstudio-engine/window/windowing_native/layer_attach_stub.c
+++ mgstudio-engine/window/windowing_native/layer_attach_stub.c
@@ -19,6 +19,21 @@
 
 typedef unsigned long mgstudio_ns_uint;
 
+typedef struct {
+  double x;
+  double y;
+} mgstudio_cg_point;
+
+typedef struct {
+  double width;
+  double height;
+} mgstudio_cg_size;
+
+typedef struct {
+  mgstudio_cg_point origin;
+  mgstudio_cg_size size;
+} mgstudio_cg_rect;
+
 static id mgstudio_find_content_view_by_raw_id(int32_t target_raw_id) {
   if (target_raw_id <= 0) {
     return (id)0;
@@ -97,6 +112,57 @@ void mgstudio_window_attach_metal_layer(int32_t raw_window_id, void *layer) {
     return;
   }
 
+  double scale = 1.0;
+  id window = ((id(*)(id, SEL))objc_msgSend)(
+    content_view,
+    sel_registerName("window"));
+  if (window) {
+    scale = ((double(*)(id, SEL))objc_msgSend)(
+      window,
+      sel_registerName("backingScaleFactor"));
+  }
+  if (scale <= 0.0) {
+    Class screen_class = (Class)objc_getClass("NSScreen");
+    if (screen_class) {
+      id screen = ((id(*)(id, SEL))objc_msgSend)(
+        (id)screen_class,
+        sel_registerName("mainScreen"));
+      if (screen) {
+        scale = ((double(*)(id, SEL))objc_msgSend)(
+          screen,
+          sel_registerName("backingScaleFactor"));
+      }
+    }
+  }
+  if (scale <= 0.0) {
+    scale = 1.0;
+  }
+
+  mgstudio_cg_rect bounds = ((mgstudio_cg_rect(*)(id, SEL))objc_msgSend)(
+    content_view,
+    sel_registerName("bounds"));
+
+  ((void(*)(id, SEL, double))objc_msgSend)(
+    (id)layer,
+    sel_registerName("setContentsScale:"),
+    scale);
+  ((void(*)(id, SEL, mgstudio_cg_rect))objc_msgSend)(
+    (id)layer,
+    sel_registerName("setFrame:"),
+    bounds);
+  mgstudio_cg_size drawable_size = {
+    bounds.size.width * scale,
+    bounds.size.height * scale,
+  };
+  ((void(*)(id, SEL, mgstudio_cg_size))objc_msgSend)(
+    (id)layer,
+    sel_registerName("setDrawableSize:"),
+    drawable_size);
+  ((void(*)(id, SEL, mgstudio_ns_uint))objc_msgSend)(
+    (id)layer,
+    sel_registerName("setAutoresizingMask:"),
+    (mgstudio_ns_uint)((1u << 1) | (1u << 4)));
+
   ((void(*)(id, SEL, BOOL))objc_msgSend)(
     content_view,
     sel_registerName("setWantsLayer:"),
@@ -105,6 +171,34 @@ void mgstudio_window_attach_metal_layer(int32_t raw_window_id, void *layer) {
     content_view,
     sel_registerName("setLayer:"),
     (id)layer);
+  window = ((id(*)(id, SEL))objc_msgSend)(
+    content_view,
+    sel_registerName("window"));
+  if (window) {
+    double attached_scale = ((double(*)(id, SEL))objc_msgSend)(
+      window,
+      sel_registerName("backingScaleFactor"));
+    if (attached_scale > 0.0) {
+      scale = attached_scale;
+    }
+  }
+  bounds = ((mgstudio_cg_rect(*)(id, SEL))objc_msgSend)(
+    content_view,
+    sel_registerName("bounds"));
+  drawable_size.width = bounds.size.width * scale;
+  drawable_size.height = bounds.size.height * scale;
+  ((void(*)(id, SEL, double))objc_msgSend)(
+    (id)layer,
+    sel_registerName("setContentsScale:"),
+    scale);
+  ((void(*)(id, SEL, mgstudio_cg_rect))objc_msgSend)(
+    (id)layer,
+    sel_registerName("setFrame:"),
+    bounds);
+  ((void(*)(id, SEL, mgstudio_cg_size))objc_msgSend)(
+    (id)layer,
+    sel_registerName("setDrawableSize:"),
+    drawable_size);
 #else
   (void)raw_window_id;
   (void)layer;
